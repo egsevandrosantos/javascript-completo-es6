7a47b9c829d9abe96344fa6814fd911b4aa239e2
Evandro Gabriel dos Santos <egs.evandro.santos@gmail.com>
2020-07-17 12:47:14 -0300

    0201 Variaveis

---
+++ variaveis/index.js
@@ -0,0 +1,30 @@
+var nome = "André";
+var idade = 28;
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = preco * totalComprado;
+
+console.log(preco, totalComprado, totalPreco);
+
+var sobrenome = "Rafael",
+  cidade = "Rio";
+
+console.log(sobrenome, cidade);
+
+var semDefinir;
+
+console.log(semDefinir);
+
+console.log(definiDepois);
+var definiDepois = "Defini depois"; // Hoisting
+console.log(definiDepois);
+
+var time = "Vasco";
+time = "Flamengo";
+console.log(time);
+
+// console.log(aindaNaoDefini); // Erro
