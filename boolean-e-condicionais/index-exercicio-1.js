d1a6f154d5990b7883c41352e7d8f8926f8be33d
Evandro Gabriel dos Santos <egs.evandro.santos@gmail.com>
2020-07-17 17:38:02 -0300

    0204 Boolean e condicionais

---
+++ boolean-e-condicionais/index-exercicio-1.js
@@ -0,0 +1,55 @@
+// Verifique se a sua idade é maior do que a de algum parente
+// Dependendo do resultado coloque no console 'É maior', 'É igual' ou 'É menor'
+var minhaIdade = 21,
+  idadePrimoMaisVelho = 22;
+if (minhaIdade > idadePrimoMaisVelho) {
+  console.log("É maior");
+} else if (minhaIdade === idadePrimoMaisVelho) {
+  console.log("É igual");
+} else {
+  console.log("É menor");
+}
+
+// Qual valor é retornado na seguinte expressão?
+var expressao = 5 - 2 && 5 - " " && 5 - 2; // 3
+console.log(expressao);
+
+// Verifique se as seguintes variáveis são Truthy ou Falsy
+var nome = "Andre"; // true
+var idade = 28; // true
+var possuiDoutorado = false; // false
+var empregoFuturo; // false
+var dinheiroNaConta = 0; // false
+console.log(
+  !!nome,
+  !!idade,
+  !!possuiDoutorado,
+  !!empregoFuturo,
+  !!dinheiroNaConta
+);
+
+// Compare o total de habitantes do Brasil com China (valor em milhões)
+var brasil = 207;
+var china = 1340;
+
+if (china > brasil) {
+  console.log("China maior");
+} else if (china === brasil) {
+  console.log("Iguais");
+} else {
+  console.log("Brasil maior");
+}
+
+// O que irá aparecer no console?
+if ("Gato" === "gato" && 5 > 2) {
+  console.log("Verdadeiro");
+} else {
+  console.log("Falso"); // isso
+}
+
+// O que irá aparecer no console?
+if ("Gato" === "gato" || 5 > 2) {
+  console.log("Gato" && "Cão"); // Cão
+} else {
+  console.log("Falso");
+}
