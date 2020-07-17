7cef8dc31e235b76c0b6c6535ce86780875a0869
egsevandrosantos <36854417+egsevandrosantos@users.noreply.github.com>
2020-07-17 12:49:04 -0300

    0201 Variaveis (#1)

---
+++ variaveis/index-exercicio-1.js
@@ -0,0 +1,16 @@
+// Exercicios
+// Declarar uma variável com seu nome
+var meuNome = "Evandro";
+console.log(meuNome);
+// Declarar uma variável com sua idade
+var minhaIdade = 21;
+console.log(minhaIdade);
+// Declarar uma variável com sua comida preferida e não atribuir valor
+var minhaComidaPreferida;
+console.log(minhaComidaPreferida);
+// Atribuir valor a sua comida preferida
+minhaComidaPreferida = "Lasanha";
+console.log(minhaComidaPreferida);
+// Declarar 5 variaveis diferentes sem valores
+var nomeMae, nomePai, saoCasados, idadeMae, idadePai;
+console.log(nomeMae, nomePai, saoCasados, idadeMae, idadePai);
