fc4fa8387a4409ec41daf6f8549c8cefe76502f4
Evandro Gabriel dos Santos <egs.evandro.santos@gmail.com>
2020-07-17 13:09:39 -0300

    0202 Tipos de dados

---
+++ tipos-de-dados/index-exercicio-1.js
@@ -0,0 +1,21 @@
+// Declare uma variável contendo uma string
+var bla = "Bla";
+console.log(bla);
+// Declare uma variável contendo um número dentro de uma string
+var idade = "18";
+console.log(idade);
+// Declare uma variável com a sua idade
+var minhaIdade = 21;
+console.log(minhaIdade);
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var nome = "Evandro",
+  sobrenome = "Santos";
+console.log(nome + " " + sobrenome);
+console.log(`${nome} ${sobrenome}`);
+// Coloque a seguinte frase em uma variável: It's time
+var its = "It's time";
+console.log(its);
+// Verifique o tipo da variável que contém o seu nome
+var tipoNome = typeof nome; // "string" | "number" | "bigint" | "boolean" | "symbol" | "undefined" | "object" | "function"
+console.log(typeof nome, tipoNome);
