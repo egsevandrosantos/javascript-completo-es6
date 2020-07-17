1cad31e181eeff530a06b400fe6be58c1c418fc3
egsevandrosantos <36854417+egsevandrosantos@users.noreply.github.com>
2020-07-17 15:32:29 -0300

    0203 Numeros e operadores (#3)

---
+++ numeros-e-operadores/index.js
@@ -0,0 +1,28 @@
+var idade = 23;
+console.log(idade);
+
+var total1 = 10 + 5 + 10;
+var divisao = 200 / 5;
+console.log(total1, divisao);
+
+var testeNaN = "Isso é 100" / 2;
+console.log(isNaN(testeNaN));
+
+var soma1 = 10 + 10 + 20 + (30 * 4) / (2 + 10);
+console.log(soma1);
+
+var incremento = 1;
+console.log(incremento++);
+console.log(incremento);
+console.log(++incremento);
+console.log(incremento);
+
+var decremento = 10;
+console.log(decremento--);
+console.log(decremento);
+console.log(--decremento);
+console.log(decremento);
+
+var idade = "28";
+var somaIdade = 5;
+console.log(+idade + somaIdade);
