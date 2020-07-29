c679f5edfd92cbf9cec84df8ec9010781601562b
egsevandrosantos <36854417+egsevandrosantos@users.noreply.github.com>
2020-07-29 13:47:42 -0300

    0402 Prototype (#28)

---
+++ objetos/prototype/index-exercicio-1.js
@@ -0,0 +1,37 @@
+// Crie uma função construtora de Pessoas
+// Deve conter nome, sobrenome e idade
+// Crie um método no protótipo que retorne
+// o nome completo da pessoa
+function Pessoa(nome, sobrenome, idade) {
+  this.nome = nome;
+  this.sobrenome = sobrenome;
+  this.idade = idade;
+}
+Pessoa.prototype.nomeCompleto = function () {
+  return `${this.nome} ${this.sobrenome}`;
+};
+
+const evandro = new Pessoa("Evandro", "Santos", 21);
+console.log(evandro.nomeCompleto());
+
+// Liste os métodos acessados por
+// dados criados com NodeList,
+// HTMLCollection, Document
+console.log(Object.getOwnPropertyNames(NodeList.prototype));
+console.log(Object.getOwnPropertyNames(HTMLCollection.prototype));
+console.log(Object.getOwnPropertyNames(Document.prototype));
+
+// Liste os construtores dos dados abaixo
+const li = document.querySelector("li");
+
+console.log(li.constructor.name);
+console.log(li.click.constructor.name);
+console.log(li.innerText.constructor.name);
+console.log(li.value.constructor.name);
+console.log(li.hidden.constructor.name);
+console.log(li.offsetLeft.constructor.name);
+console.log(li.click()); // undefined
+
+// Qual o construtor do dado abaixo:
+li.hidden.constructor.name; // "Boolean"
+console.log(li.hidden.constructor.name.constructor.name); // String
