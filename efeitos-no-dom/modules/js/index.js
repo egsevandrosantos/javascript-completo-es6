14ac7bbe5cdf46bb2aaeda98e4e7058efaae93a4
Evandro Gabriel dos Santos <egs.evandro.santos@gmail.com>
2020-08-17 15:44:51 -0300

    0502 Modules

---
+++ efeitos-no-dom/modules/js/index.js
@@ -0,0 +1,13 @@
+import initScrollSuave from "./modules/scroll-suave.js";
+import initScrollAnimacao from "./modules/scroll-animacao.js";
+import initAccordionList from "./modules/accordion.js";
+import initTabNav from "./modules/tabnav.js";
+// import { teste1, teste2 as testeFinal } from "./modules/teste.js";
+import * as teste from "./modules/teste.js";
+
+initScrollSuave();
+initScrollAnimacao();
+initAccordionList();
+initTabNav();
+teste.teste1();
+teste.teste2();
