5faabab620a3fb3992be0c9146bf97cf7b72c00c
egsevandrosantos <36854417+egsevandrosantos@users.noreply.github.com>
2020-08-22 17:20:14 -0300

    0510 Anima numeros (#44)

---
+++ efeitos-no-dom/anima-numeros/js/modules/scroll-animacao.js
@@ -0,0 +1,20 @@
+export default function initAnimacaoScroll() {
+  const sections = document.querySelectorAll('[data-js="scroll"]');
+  if (sections.length) {
+    const window60Perc = window.innerHeight * 0.6;
+
+    function animaScroll() {
+      sections.forEach((item) => {
+        const sectionTop = item.getBoundingClientRect().top - window60Perc;
+        if (sectionTop <= 0) {
+          item.classList.add("ativo");
+        } else if (item.classList.contains("ativo")) {
+          item.classList.remove("ativo");
+        }
+      });
+    }
+    animaScroll();
+
+    window.addEventListener("scroll", animaScroll);
+  }
+}
