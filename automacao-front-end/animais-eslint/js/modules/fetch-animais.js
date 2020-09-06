026ca4f994a253f9c0586e3f9e9de5b39b960cec
egsevandrosantos <36854417+egsevandrosantos@users.noreply.github.com>
2020-09-06 18:25:03 -0300

    1003 ESLint 2 (#72)

---
+++ automacao-front-end/animais-eslint/js/modules/fetch-animais.js
@@ -0,0 +1,27 @@
+import initAnimaNumeros from "./anima-numeros.js";
+
+export default function initFetchAnimais() {
+  function createAnimal(animal) {
+    const div = document.createElement("div");
+    div.classList.add("numero-animal");
+    div.innerHTML = `<h3>${animal.specie}</h3><span data-numero>${animal.total}</span>`;
+    return div;
+  }
+
+  async function fetchAnimais(url) {
+    try {
+      const animaisFetch = await fetch(url);
+      const animaisJson = await animaisFetch.json();
+      const numerosGrid = document.querySelector(".numeros-grid");
+      animaisJson.forEach((item) => {
+        const div = createAnimal(item);
+        numerosGrid.appendChild(div);
+      });
+      initAnimaNumeros();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  fetchAnimais("./animaisapi.json");
+}
