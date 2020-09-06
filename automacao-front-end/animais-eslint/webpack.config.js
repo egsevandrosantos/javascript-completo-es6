95b9a038dfdc8c5b3581dbd59596519ece3daeb2
egsevandrosantos <36854417+egsevandrosantos@users.noreply.github.com>
2020-09-06 20:43:46 -0300

    1005 Babel (#74)

---
+++ automacao-front-end/animais-eslint/webpack.config.js
@@ -0,0 +1,9 @@
+const path = require("path");
+
+module.exports = {
+  entry: ["@babel/polyfill", "whatwg-fetch", "./js/index.js"],
+  output: {
+    path: path.resolve(__dirname, "./dist/"),
+    filename: "main.js",
+  },
+};
