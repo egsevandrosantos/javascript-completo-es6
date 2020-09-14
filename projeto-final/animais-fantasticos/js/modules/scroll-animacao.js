1b6a2b4b296904916714c0d16d0a45a1435fc0f7
Evandro Gabriel dos Santos <egs.evandro.santos@gmail.com>
2020-09-14 20:18:34 -0300

    1110 Refazer animacao scroll pt. 2

--- projeto-final/animais-fantasticos/js/modules/scroll-animacao.js
+++ projeto-final/animais-fantasticos/js/modules/scroll-animacao.js
@@ -1,21 +1,31 @@
 export default class AnimacaoScroll {
   constructor(sections) {
-    this.animaScroll = this.animaScroll.bind(this);
+    this.checkDistance = this.checkDistance.bind(this);
     this.sections = document.querySelectorAll(sections);
     this.window60Perc = window.innerHeight * 0.6;
     if (this.sections.length) {
-      this.animaScroll();
-      window.addEventListener("scroll", this.animaScroll);
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener("scroll", this.checkDistance);
     }
   }
 
-  animaScroll() {
-    this.sections.forEach((item) => {
-      const sectionTop = item.getBoundingClientRect().top - this.window60Perc;
-      if (sectionTop <= 0) {
-        item.classList.add("ativo");
-      } else if (item.classList.contains("ativo")) {
-        item.classList.remove("ativo");
+  getDistance() {
+    this.distance = [...this.sections].map((item) => {
+      const { offsetTop } = item;
+      return {
+        element: item,
+        offsetTop,
+      };
+    });
+  }
+
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.element.offsetTop - this.window60Perc) {
+        item.element.classList.add("ativo");
+      } else if (item.element.classList.contains("ativo")) {
+        item.element.classList.remove("ativo");
       }
     });
   }
