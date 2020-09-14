ab13e5c8448b42ff576eeb3a88c974325d866568
Evandro Gabriel dos Santos <egs.evandro.santos@gmail.com>
2020-09-14 20:00:12 -0300

    1110 Refazer animacao scroll pt. 1

--- projeto-final/animais-fantasticos/js/modules/scroll-animacao.js
+++ projeto-final/animais-fantasticos/js/modules/scroll-animacao.js
@@ -1,20 +1,22 @@
-export default function initAnimacaoScroll() {
-  const sections = document.querySelectorAll('[data-js="scroll"]');
-  const window60Perc = window.innerHeight * 0.6;
+export default class AnimacaoScroll {
+  constructor(sections) {
+    this.animaScroll = this.animaScroll.bind(this);
+    this.sections = document.querySelectorAll(sections);
+    this.window60Perc = window.innerHeight * 0.6;
+    if (this.sections.length) {
+      this.animaScroll();
+      window.addEventListener("scroll", this.animaScroll);
+    }
+  }
 
-  function animaScroll() {
-    sections.forEach((item) => {
-      const sectionTop = item.getBoundingClientRect().top - window60Perc;
+  animaScroll() {
+    this.sections.forEach((item) => {
+      const sectionTop = item.getBoundingClientRect().top - this.window60Perc;
       if (sectionTop <= 0) {
         item.classList.add("ativo");
       } else if (item.classList.contains("ativo")) {
         item.classList.remove("ativo");
       }
     });
-  }
-
-  if (sections.length) {
-    animaScroll();
-    window.addEventListener("scroll", animaScroll);
   }
 }
