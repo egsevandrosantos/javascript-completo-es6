4abfb05932e5af7d6699451e834c45abc4f369b6
Evandro Gabriel dos Santos <egs.evandro.santos@gmail.com>
2020-09-14 06:29:57 -0300

    1106 Refatorar tooltip

--- projeto-final/animais-fantasticos/js/modules/tooltip.js
+++ projeto-final/animais-fantasticos/js/modules/tooltip.js
@@ -1,7 +1,14 @@
-export default function initTooltip() {
-  const tooltips = document.querySelectorAll("[data-tooltip]");
+export default class Tooltip {
+  constructor(tooltip) {
+    this.onMouseOver = this.onMouseOver.bind(this);
+    this.criarFuncoesObjeto();
+    this.tooltips = document.querySelectorAll(tooltip);
+    this.tooltips.forEach((item) => {
+      item.addEventListener("mouseover", this.onMouseOver);
+    });
+  }
 
-  function criarTooltipBox(element) {
+  static criarTooltipBox(element) {
     const text = element.getAttribute("aria-label");
     const tooltipBox = document.createElement("div");
     tooltipBox.classList.add("tooltip");
@@ -10,40 +17,49 @@ export default function initTooltip() {
     return tooltipBox;
   }
 
-  const onMouseMove = {
-    tooltipBox: null,
-    handleEvent(event) {
-      this.tooltipBox.style.top = `${event.pageY + 20}px`;
-      this.tooltipBox.style.left = `${event.pageX + 20}px`;
-    },
-  };
-
-  const onMouseLeave = {
-    // Para criar um objeto como callback, deve ter o método handleEvent
-    tooltipBox: null,
-    element: null,
-    handleEvent() {
-      if (this.tooltipBox && this.element) {
-        this.tooltipBox.remove();
-        this.element.removeEventListener("mouseleave", onMouseLeave);
-        this.element.removeEventListener("mousemove", onMouseMove);
-      }
-    },
-  };
-
-  function onMouseOver(event) {
-    const tooltipBox = criarTooltipBox(this);
-    tooltipBox.style.top = `${event.pageY}px`;
-    tooltipBox.style.left = `${event.pageX}px`;
-
-    onMouseLeave.tooltipBox = tooltipBox;
-    onMouseMove.tooltipBox = tooltipBox;
-    onMouseLeave.element = this;
-    this.addEventListener("mouseleave", onMouseLeave);
-    this.addEventListener("mousemove", onMouseMove);
+  createOnMouseMove() {
+    this.onMouseMove = {
+      tooltipBox: null,
+      handleEvent({ pageY, pageX }) {
+        this.tooltipBox.style.top = `${pageY + 20}px`;
+        if (pageX + 240 > window.innerWidth) {
+          this.tooltipBox.style.left = `${pageX - 150}px`;
+        } else {
+          this.tooltipBox.style.left = `${pageX + 20}px`;
+        }
+      },
+    };
   }
 
-  tooltips.forEach((item) => {
-    item.addEventListener("mouseover", onMouseOver);
-  });
+  createOnMouseLeave() {
+    this.onMouseLeave = {
+      // Para criar um objeto como callback, deve ter o método handleEvent
+      tooltipBox: null,
+      element: null,
+      handleEvent() {
+        if (this.tooltipBox && this.element) {
+          this.tooltipBox.remove();
+          this.element.removeEventListener("mouseleave", this.onMouseLeave);
+          this.element.removeEventListener("mousemove", this.onMouseMove);
+        }
+      },
+    };
+  }
+
+  criarFuncoesObjeto() {
+    this.createOnMouseMove();
+    this.createOnMouseLeave();
+  }
+
+  onMouseOver({ currentTarget, pageY, pageX }) {
+    const tooltipBox = Tooltip.criarTooltipBox(currentTarget);
+    tooltipBox.style.top = `${pageY}px`;
+    tooltipBox.style.left = `${pageX}px`;
+
+    this.onMouseLeave.tooltipBox = tooltipBox;
+    this.onMouseMove.tooltipBox = tooltipBox;
+    this.onMouseLeave.element = currentTarget;
+    currentTarget.addEventListener("mouseleave", this.onMouseLeave);
+    currentTarget.addEventListener("mousemove", this.onMouseMove);
+  }
 }
