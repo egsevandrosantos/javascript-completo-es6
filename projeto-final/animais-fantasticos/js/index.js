4a552b2a0bd3ab8b2c2074019bbc2f24da9a179b
Evandro Gabriel dos Santos <egs.evandro.santos@gmail.com>
2020-09-14 21:49:34 -0300

    1112 Refatorar dropdown menu

--- projeto-final/animais-fantasticos/js/index.js
+++ projeto-final/animais-fantasticos/js/index.js
@@ -6,7 +6,7 @@ import AccordionList from "./modules/accordion.js";
 import TabNav from "./modules/tabnav.js";
 import Modal from "./modules/modal.js";
 import Tooltip from "./modules/tooltip.js";
-import initDropdownMenu from "./modules/dropdown-menu.js";
+import DropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
 import initFetchAnimais from "./modules/fetch-animais.js";
@@ -24,7 +24,7 @@ import initFetchBitcoin from "./modules/fetch-bitcoin.js";
     '[data-modal="container"]'
   ))();
 (() => new Tooltip("[data-tooltip]"))();
-initDropdownMenu();
+(() => new DropdownMenu("[data-dropdown]"))();
 initMenuMobile();
 initFuncionamento();
 initFetchAnimais();
