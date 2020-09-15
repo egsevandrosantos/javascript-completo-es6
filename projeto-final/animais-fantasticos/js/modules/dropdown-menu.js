6b71579acd35361fc9050b90925ef48446a44315
egsevandrosantos <36854417+egsevandrosantos@users.noreply.github.com>
2020-09-14 21:50:03 -0300

    1112 Refatorar dropdown menu (#87)

--- projeto-final/animais-fantasticos/js/modules/dropdown-menu.js
+++ projeto-final/animais-fantasticos/js/modules/dropdown-menu.js
@@ -1,28 +1,36 @@
 import outsideClick from "./outsideclick.js";
 
-export default function initDropdownMenu() {
-  const dropdownMenus = document.querySelectorAll("[data-dropdown]");
+export default class DropdownMenu {
+  constructor(dropdownMenus) {
+    this.dropdownMenus = document.querySelectorAll(dropdownMenus);
+    this.criarFuncoesObj();
+    this.dropdownMenus.forEach((item) => {
+      // item.addEventListener("touchstart", handleClick);
+      // item.addEventListener("click", handleClick);
+      const events = ["touchstart", "click"];
+      this.handleClick.events = events;
+      events.forEach((itemEvent) =>
+        item.addEventListener(itemEvent, this.handleClick)
+      );
+    });
+  }
 
-  const handleClick = {
-    events: null,
-    element: null,
-    handleEvent(event) {
-      event.preventDefault();
-      this.element = event.currentTarget;
-      event.currentTarget.classList.add("ativo");
-      outsideClick(event.currentTarget, this.events, () => {
-        this.element.classList.remove("ativo");
-      });
-    },
-  };
+  criarFuncaoHandleClick() {
+    this.handleClick = {
+      events: null,
+      element: null,
+      handleEvent(event) {
+        event.preventDefault();
+        this.element = event.currentTarget;
+        event.currentTarget.classList.add("ativo");
+        outsideClick(event.currentTarget, this.events, () => {
+          this.element.classList.remove("ativo");
+        });
+      },
+    };
+  }
 
-  dropdownMenus.forEach((item) => {
-    // item.addEventListener("touchstart", handleClick);
-    // item.addEventListener("click", handleClick);
-    const events = ["touchstart", "click"];
-    handleClick.events = events;
-    events.forEach((itemEvent) =>
-      item.addEventListener(itemEvent, handleClick)
-    );
-  });
+  criarFuncoesObj() {
+    this.criarFuncaoHandleClick();
+  }
 }
