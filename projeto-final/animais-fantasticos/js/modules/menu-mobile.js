7b6756a46aca5226f62b8f647a0ac49a388128e1
egsevandrosantos <36854417+egsevandrosantos@users.noreply.github.com>
2020-09-16 08:29:26 -0300

    1113 Refatorar menu mobile (#88)

--- projeto-final/animais-fantasticos/js/modules/menu-mobile.js
+++ projeto-final/animais-fantasticos/js/modules/menu-mobile.js
@@ -1,22 +1,24 @@
 import outsideClick from "./outsideclick.js";
 
-export default function initMenuMobile() {
-  const menuButton = document.querySelector('[data-js="button-menu"]');
-  const menuList = document.querySelector("#menu");
-  const events = ["click", "touchstart"];
-
-  function openMenu() {
-    menuList.classList.add("ativo");
-    menuButton.classList.add("ativo");
-    outsideClick(menuList, events, () => {
-      menuList.classList.remove("ativo");
-      menuButton.classList.remove("ativo");
-    });
+export default class MenuMobile {
+  constructor(menuButton, menu) {
+    this.openMenu = this.openMenu.bind(this);
+    this.menuButton = document.querySelector(menuButton);
+    this.menuList = document.querySelector(menu);
+    this.events = ["click", "touchstart"];
+    if (this.menuButton && this.menuList) {
+      this.events.forEach((itemEvent) => {
+        this.menuButton.addEventListener(itemEvent, this.openMenu);
+      });
+    }
   }
 
-  if (menuButton && menuList) {
-    events.forEach((itemEvent) => {
-      menuButton.addEventListener(itemEvent, openMenu);
+  openMenu() {
+    this.menuList.classList.add("ativo");
+    this.menuButton.classList.add("ativo");
+    outsideClick(this.menuList, this.events, () => {
+      this.menuList.classList.remove("ativo");
+      this.menuButton.classList.remove("ativo");
     });
   }
 }
