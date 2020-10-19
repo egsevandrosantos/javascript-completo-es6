2cd244327c8fdc7aac4a1e495d6469e0e0fadc41
Evandro Santos <egs.evandro.santos@gmail.com>
2020-10-19 19:15:39 -0300

    Removendo console log

--- slide/js/index.js
+++ slide/js/index.js
@@ -3,4 +3,3 @@ import SlideNav from "./slide-nav.js";
 const slideNav = new SlideNav(".slide", ".wrapper");
 slideNav.changeSlide(0);
 slideNav.addArrow(".arrow-previous", ".arrow-next");
-console.log(slideNav);
