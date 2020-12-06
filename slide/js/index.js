f683fdbe0baa6a0d578248034fe026b8ddec79b5
Evandro Santos <egs.evandro.santos@gmail.com>
2020-12-06 19:40:39 -0300

    1210 Slide custom controls

--- slide/js/index.js
+++ slide/js/index.js
@@ -3,4 +3,4 @@ import SlideNav from "./slide-nav.js";
 const slideNav = new SlideNav(".slide", ".wrapper");
 slideNav.changeSlide(0);
 slideNav.addArrow(".arrow-previous", ".arrow-next");
-slideNav.addControl();
+slideNav.addControl(".custom-controls");
